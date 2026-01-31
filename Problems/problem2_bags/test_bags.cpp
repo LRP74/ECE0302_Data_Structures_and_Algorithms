18679dcfdc2cf41479f878ef2823b95a89ed0101
leroy <lrp74@pitt.edu>
2026-01-31 17:21:37 -0500

    sorted test cases into individual ones

--- Problems/problem2_bags/test_bags.cpp
+++ Problems/problem2_bags/test_bags.cpp
@@ -34,12 +34,16 @@ TEST_CASE("Calling all public methods of DynamicBag", "[DynamicBag]")
 // My Limited bag test cases
 // test cases should check functionality of add, remove, isEmpty, getsizeof, contains, clear, frequency, and = overload
 //
-TEST_CASE("Testing limited bag X functions for basic functionality", "[LimitedSizeBag]")
+TEST_CASE("Testing limited bag isEmpty and currentSize functions for basic functionality", "[LimitedSizeBag]")
 {
   LimitedSizeBag<int> L1;
 
   REQUIRE(L1.isEmpty() == true);  //default bag is empty
   REQUIRE(L1.getCurrentSize() == 0);  //default bag size is 0
+}
+TEST_CASE("Testing limited bag addd and currentSize functions for basic functionality", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> L1;  
 
   for (int i = 0; i < 5; i++) //add 5 items
   {
@@ -52,10 +56,28 @@ TEST_CASE("Testing limited bag X functions for basic functionality", "[LimitedSi
   }
   
   REQUIRE(L1.getCurrentSize() == 5);  //check size is 5
+}
+TEST_CASE("Testing limited bag getFrequency functions for basic functionality", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> L1;
+  for (int i = 0; i < 5; i++) //add 5 items
+  {
+    L1.add(i + 1);
+  }
   
   for (int i = 0; i < 5; i++) //check frequency of each item is 1
   {
     REQUIRE(L1.getFrequencyOf(i + 1) == 1);
+  }
+}
+
+TEST_CASE("Testing limited bag remove functions for basic functionality", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> L1;
+
+  for (int i = 0; i < 5; i++) //add 5 items
+  {
+    L1.add(i + 1);
   }
 
   L1.remove(3); //remove item 3
@@ -64,6 +86,16 @@ TEST_CASE("Testing limited bag X functions for basic functionality", "[LimitedSi
   REQUIRE(L1.contains(3) == false); //make sure 3 is gone
 
   REQUIRE(L1.add(7) == true); //add another element
+}
+  
+TEST_CASE("Testing limited bag assignment operator for basic functionality", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> L1;
+
+  for (int i = 0; i < 5; i++) //add 5 items
+  {
+    L1.add(i + 1);
+  }
 
   LimitedSizeBag<int> L2;
   L2 = L1;
@@ -71,18 +103,40 @@ TEST_CASE("Testing limited bag X functions for basic functionality", "[LimitedSi
   REQUIRE((L1 == L2) == true);  //test assignment operator
 }
 
-TEST_CASE("Test limited bag functions for edge, error, or invalid cases", "[LimitedSizeBag]")
+TEST_CASE("Test limited bag for clear function", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> L1;
+
+  for (int i = 0; i < 5; i++) //add 5 items
+  {
+    L1.add(i + 1);
+  }
+
+  L1.clear();
+  REQUIRE(L1.isEmpty() == true); //check if clear worked
+}
+
+TEST_CASE("Test limited bag functions for removing from empty bag", "[LimitedSizeBag]")
 {
   LimitedSizeBag<int> L1;
 
   REQUIRE(L1.remove(10) == false); //removing from empty bag
+}
 
+TEST_CASE("Test limited bag functions for adding to full bag", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> L1;
   for (int i = 0; i < LimitedSizeBag<int>::maxsize; i++)
   {
     L1.add(i);
   }
 
   REQUIRE(L1.add(101) == false); //adding to full bag
+}
+
+TEST_CASE("Test limited bag functions for getting frequency of item not in bag", "[LimitedSizeBag]")
+{
+  LimitedSizeBag<int> L1;
 
   REQUIRE(L1.getFrequencyOf(200) == 0); //frequency of item not in bag
 
@@ -131,63 +185,92 @@ TEST_CASE("Testing limited bag X functions for basic functionality using doubles
 // My Dynamic bag test cases
 // test cases should check functionality of add, remove, isEmpty, getsizeof, contains, clear, frequency, and = overload
 //
-TEST_CASE("Testing dynamic bag X functions for basic functionality", "[DynamicBag]")
+TEST_CASE("Testing dynamic bag isEmpty and currentSize functions for basic functionality", "[DynamicBag]")
 {
   DynamicBag<int> D1;
 
   REQUIRE(D1.isEmpty() == true);  //default bag is empty
   REQUIRE(D1.getCurrentSize() == 0);  //default bag size is 0
+}
 
+TEST_CASE("Testing dynamic bag add and currentSize functions for basic functionality", "[DynamicBag]")
+{
+  DynamicBag<int> D1;
   for (int i = 0; i < 5; i++) //add 5 items
   {
     D1.add(i + 1);
   }
-  
   for (int i = 0; i < 5; i++) //check if they are in the bag
   {
     REQUIRE(D1.contains(i + 1) == true);
   }
-  
   REQUIRE(D1.getCurrentSize() == 5);  //check size is 5
-  
+}
+
+TEST_CASE("Testing dynamic bag getFrequency functions for basic functionality", "[DynamicBag]")
+{
+  DynamicBag<int> D1;
+  for (int i = 0; i < 5; i++) //add 5 items
+  {
+    D1.add(i + 1);
+  }
   for (int i = 0; i < 5; i++) //check frequency of each item is 1
   {
     REQUIRE(D1.getFrequencyOf(i + 1) == 1);
   }
-
+}
+TEST_CASE("Testing dynamic bag remove functions for basic functionality", "[DynamicBag]")
+{
+  DynamicBag<int> D1;
+  for (int i = 0; i < 5; i++) //add 5 items
+  {
+    D1.add(i + 1);
+  }
   D1.remove(3); //remove item 3
-
   REQUIRE(D1.getCurrentSize() == 4);  //check size again
   REQUIRE(D1.contains(3) == false); //make sure 3 is gone
-
   REQUIRE(D1.add(7) == true); //add another element
-
+}
+TEST_CASE("Testing dynamic bag assignment operator for basic functionality", "[DynamicBag]")
+{
+  DynamicBag<int> D1;
+  for (int i = 0; i < 5; i++) //add 5 items
+  {
+    D1.add(i + 1);
+  }
   DynamicBag<int> D2;
   D2 = D1;
-
   REQUIRE((D1 == D2) == true);  //test assignment operator
 }
+TEST_CASE("Test dynamic bag for clear function", "[DynamicBag]")
+{
+  DynamicBag<int> D1;
+  for (int i = 0; i < 5; i++) //add 5 items
+  {
+    D1.add(i + 1);
+  }
+  D1.clear();
+  REQUIRE(D1.isEmpty() == true); //check if clear worked
+}
 
-TEST_CASE("Test dynamic bag functions for edge, error, or invalid cases", "[DynamicBag]")
+TEST_CASE("Test dynamic bag functions for removing from empty bag", "[DynamicBag]")
 {
   DynamicBag<int> D1;
 
   REQUIRE(D1.remove(10) == false); //removing from empty bag
+}
 
-  for (int i = 0; i < 200; i++) //add more than initial capacity to test resizing
-  {
-    D1.add(i);
-  }
+TEST_CASE("Test dynamic bag functions for getting frequency of item not in bag", "[DynamicBag]")
+{
+  DynamicBag<int> D1;
 
-  REQUIRE(D1.getCurrentSize() == 200); //check size after adding
-
-  REQUIRE(D1.getFrequencyOf(300) == 0); //frequency of item not in bag
+  REQUIRE(D1.getFrequencyOf(200) == 0); //frequency of item not in bag
 
   D1.clear();
   REQUIRE(D1.isEmpty() == true); //check if clear worked
 }
 
-TEST_CASE("Testing dynamic bag X functions for basic functionality using doubles", "[DynamicBag]")
+TEST_CASE("Testing dynamic bag functions for basic functionality using doubles", "[DynamicBag]")
 {
   DynamicBag<double> D1;
 
